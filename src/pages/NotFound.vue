52b90f60b7ab816b8d93e991ef269f960f63c2b7
Antonio <pavicic.antonio@gmail.com>
2021-11-21 17:33:42 +0100

    style page Not found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,30 @@
 <template>
-  <section>
-    <h2>Page not found</h2>
-    <p>
+  <section class="not-found">
+    <h2 class="not-found__title">Page not found</h2>
+    <p class="not-found__message">
       This page could not be found - maybe check out more about the app at
-      <router-link to="/">Start Page</router-link>
+      <router-link class="not-found__link" to="/">Start Page</router-link>
     </p>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.not-found {
+  text-align: center;
+  color: var(--color-grey-dark-2);
+
+  &__title {
+    font-size: 2.5rem;
+    margin: 5rem 0;
+  }
+
+  &__message {
+    font-size: 1.2rem;
+  }
+
+  &__link {
+    color: var(--color-secondary-dark);
+    text-decoration: none;
+  }
+}
+</style>
